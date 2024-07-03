67300ac1d38b00ffec7d75e275a88010b370ddf1
Kyler Gibbs <kylergibbs@hotmail.com>
2024-07-02 17:46:59 -0700

    additional routes finished.

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,12 +1,72 @@
-const User = require('../models/User')
+const User = require("../models/User");
 
 module.exports = {
-    async createUser(req, res) {
-        try {
-            const user = await User.create(req.body);
-            res.json(user);
-        } catch (err) {
-            res.status(500).json(err);
-        }
+  async createUser(req, res) {
+    try {
+      const user = await User.create(req.body);
+      res.json(user);
+    } catch (err) {
+      res.status(500).json(err);
     }
-}
+  },
+  async getUsers(req, res) {
+    try {
+      const user = await User.find();
+      res.json(user);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+  async getSingleUser(req, res) {
+    try {
+      const user = await User.findOne({ _id: req.params.userId });
+      if (!user) {
+        return res.status(404).json({ message: "No user with that ID" });
+      }
+      res.json(user);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+  async updateUser(req, res) {
+    try {
+      const user = await User.findOneAndUpdate(
+        { _id: req.params.userId },
+        { username: req.body.username, email: req.body.email },
+        { returnOriginal: false }
+      );
+      if (!user) {
+        return res.status(404).json({ message: "No user with that ID" });
+      }
+      res.json(user);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+
+  async deleteUser(req, res) {
+    try {
+      const user = await User.findOneAndDelete({ _id: req.params.userId });
+      if (!user) {
+        return res.status(404).json({ message: "No user with that ID" });
+      }
+      res.json("User deleted");
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+
+  async addFriend(req,res) {
+    try{
+        const user = await User.findOneAndUpdate(
+            {_id: req.params.userId},
+            {$push: {friends: req.params.friendId}});
+        if (!user) {
+            return res.status(404).json({ message: "No user with that ID" });
+          }
+        res.json("Friend added")    
+    } catch (err) {
+        res.status(500).json(err);
+      }
+    },
+};
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,7 +1,16 @@
 const router = require('express').Router();
 
-const {createUser} = require('../../controllers/userController');
+const {createUser, 
+       getUsers,
+       getSingleUser,
+       updateUser,
+       deleteUser,
+       addFriend } = require('../../controllers/userController');
 
-router.route('/').post(createUser);
+router.route('/').post(createUser).get(getUsers);
+
+router.route('/:userId').get(getSingleUser).put(updateUser).delete(deleteUser);
+
+router.route('/:userId/friends/:friendId').post(addFriend)
 
 module.exports = router;
